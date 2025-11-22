84092c2f84bf0d2a7f99ec83466436c5d213b4e2
Dain <sulydain85@gmail.com>
2025-11-22 19:58:03 +0100

    add projects Qrcode

--- Qrcode.py
+++ Qrcode.py
@@ -3,25 +3,19 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
 def generate_qr_pdf(data, pdf_filename="qr_code.pdf"):
-    # Générer l'image QR
     qr = qrcode.make(data)
     img_path = "temp_qr.png"
     qr.save(img_path)
 
-    # Crée le PDF avec ReportLab
     c = canvas.Canvas(pdf_filename, pagesize=A4)
     width, height = A4
 
-    # Optionnel : ajouter du texte
-    c.drawString(100, height - 100, f"QR Code pour : {'https://www.cleanertombe.fr/'}")
+    c.drawString(100, height - 100, f"QR Code pour : {'Ajoute le texte ici'}")
 
-    # Ajouter l'image QR au centre
     c.drawImage(img_path, 150, height/2 - 150, width=300, height=300)
 
-    # Sauvegarder le PDF
     c.save()
 
-    print(f"✅ PDF généré : {pdf_filename}")
+    print(f" PDF généré : {pdf_filename}")
 
-# Exemple d'utilisation
-generate_qr_pdf("https://www.cleanertombe.fr/")
+generate_qr_pdf("ici L'url")
